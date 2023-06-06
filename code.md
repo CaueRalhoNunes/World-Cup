16687386167d49b7128e7943a785c822e55507df
CaueRalhoNunes <76968286+CaueRalhoNunes@users.noreply.github.com>
2023-06-05 21:14:42 -0300

    Initial commit

---
+++ code.md
@@ -0,0 +1,23 @@
+Project Summary: Game Cards Creation and Editing System
+
+The project aims to develop a system that allows users to create and edit game cards containing information about team matches. The system will capture data such as date, time, and the teams involved.
+
+Features and Functionality:
+
+1. Card Creation: Users will be able to create new game cards by providing information such as the game date, time, and the teams participating. The system will allow users to select teams from a list of options or even upload images of team logos.
+
+2. Card Editing: In addition to creation, users will have the ability to edit existing cards. They can modify information such as the game date, time, and teams, as well as add or remove game details as needed.
+
+3. Card Viewing: The system will display the created cards in an attractive visual format, showing the game date, teams involved, and time. Users will be able to view a list of all existing cards on one page or navigate through them individually.
+
+4. User-Friendly Interface: The system will feature an intuitive and user-friendly interface. Users can interact with the cards in a simple way, creating and editing information with just a few clicks.
+
+Benefits:
+
+- Organization: The system will allow users to organize team games in a clear and visually appealing manner, facilitating game tracking.
+
+- Customization: Users will have the freedom to add and modify information in the cards, personalizing them according to their preferences.
+
+- Ease of Use: The user-friendly interface makes creating and editing cards a straightforward task, even for users with limited technical knowledge.
+
+- Quick Access: Users will be able to quickly view the cards and obtain important game information, such as date, time, and teams involved.
---
+++ javascript/mains.js
@@ -0,0 +1,255 @@
+let weekdays,
+   day,
+   setDate,
+   id = 0;
+
+const creator = document.getElementById('creator');
+const card = document.getElementById(`cardContainer`);
+const create = document.getElementById('create');
+const chooseTheCountry = document.getElementById(`chooseTheCountry`);
+const createButton = document.getElementById(`createButton`);
+const edit = document.getElementById(`edit`);
+
+const autoincrement = () => {
+   id++;
+   return id;
+};
+
+function createGame() {
+   creator.style.visibility = 'visible';
+   card.style.visibility = 'hidden';
+   create.style.visibility = 'hidden';
+   createButton.style.display = 'inline';
+   edit.style.display = 'none';
+}
+
+function closeButton() {
+   chooseTheCountry.style.visibility = 'hidden';
+   creator.style.visibility = 'visible';
+}
+
+function cancelButton() {
+   creator.style.visibility = 'hidden';
+   card.style.visibility = 'visible';
+   create.style.visibility = 'visible';
+}
+
+function showCountries() {
+   chooseTheCountry.style.visibility = 'visible';
+   create.style.visibility = 'hidden';
+   creator.style.visibility = 'hidden';
+}
+
+function pickCountry(event) {
+   const team = event.target;
+   const countrySelector = document.getElementById('countrySelector');
+   const handleCountryClick = (e) => {
+      const country = e.target.getAttribute('src');
+      team.setAttribute('src', country);
+      e.target.style.display = 'none';
+      creator.style.visibility = 'visible';
+      closeButton();
+
+      const handleSecondCountryClick = (event) => {
+         const secondCountry = event.target.getAttribute('src');
+         if (secondCountry !== country) {
+            //Here it gets the selected country and display it again
+            e.target.style.display = 'grid';
+         }
+      };
+      countrySelector.removeEventListener('click', handleCountryClick);
+      countrySelector.addEventListener('click', handleSecondCountryClick, { once: true });
+   };
+   countrySelector.addEventListener('click', handleCountryClick, { once: true });
+}
+
+function getDate() {
+   let getDateValue = document.getElementById(`date`).value;
+   const dateValueObject = new Date(getDateValue);
+   weekdays = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sabado', 'domingo'];
+   day = dateValueObject.getDay();
+   setDate = dateValueObject.toLocaleDateString('pt-BR', { timeZone: 'UTC' });
+}
+
+function deleteCard() {
+   document.addEventListener(
+      `click`,
+      (e) => {
+         const getModifiedCardButtons = e.target.parentElement;
+         const getParentElement = getModifiedCardButtons.parentElement;
+         getParentElement.remove();
+      },
+      { once: true }
+   );
+}
+
+function createCardGame() {
+   getDate();
+   autoincrement();
+
+   const images = Array.from(document.querySelectorAll('.team')).map((img) => img.getAttribute('src'));
+   const cardContainer = document.getElementById('cardContainer');
+   const newElement = document.createElement('div');
+   const firstGameHour = document.getElementById(`firstGameHour`).value;
+   const secondGameHour = document.getElementById(`secondGameHour`).value;
+   const thirdGameHour = document.getElementById(`thirdGameHour`).value;
+   newElement.classList.add('card');
+   newElement.id = id;
+   newElement.innerHTML = `
+     <header>  
+       <p class="date"> ${setDate} </p>
+       <p id="dayOfWeek"> ${weekdays[day]} </p>
+     </header>
+     
+     <div class="games">
+       <div class="col-a">
+         <ul>
+           <li>
+             <button class="icon">
+               <img src="${images[0]}" id="first" class="team"/>
+             </button>
+             <p id="firstGameHour"> ${firstGameHour} </p>
+             <button class="icon">
+               <img src="${images[1]}" id="second" class="team" />
+             </button>
+           </li>
+         </ul>
+       </div>
+       <!-- 
+
+       -->
+       <div class="col-b">
+         <ul>
+           <li>
+             <button class="icon">
+               <img src="${images[2]}" id="third" class="team" />
+             </button>
+             <p id="secondGameHour"> ${secondGameHour} </p>
+             <button class="icon">
+               <img src="${images[3]}" id="fourth" class="team" />
+             </button>
+           </li>
+         </ul>
+       </div>
+       <!-- 
+
+       -->
+       <div class="col-c">
+         <ul>
+           <li>
+             <button class="icon">
+               <img src="${images[4]}" id="fifth" class="team" />
+             </button>
+             <p id="thirdGameHour"> ${thirdGameHour} </p>
+             <button class="icon">
+               <img src="${images[5]}" id="sixth" class="team" />
+             </button>
+           </li>
+         </ul>
+       </div>
+     </div>
+     <div class="modifyCardButtons">
+       <img src="./assets/delete.svg" onclick="deleteCard()" id="deleteButton">
+       <img src="./assets/edit.svg" onclick="showEditCard()" id="editButton">
+     </div>
+     </svg>`;
+
+   cardContainer.appendChild(newElement);
+   creator.style.visibility = 'hidden';
+   create.style.visibility = 'visible';
+   cardContainer.style.visibility = 'visible';
+}
+
+function showEditCard() {
+   createGame();
+   const makeChanges = (e) => {
+      const childElement = e.target.parentElement;
+      const cardToBeEdit = childElement.parentElement;
+      const getId = cardToBeEdit.getAttribute(`id`);
+      getDate();
+      editCard(getId);
+   };
+   document.addEventListener(`click`, makeChanges, { once: true });
+   createButton.style.display = 'none';
+   edit.style.display = 'inline';
+}
+
+function editCard(id) {
+   const createEditedCard = () => {
+      getDate();
+      const images = Array.from(document.querySelectorAll('.team')).map((img) => img.getAttribute('src'));
+      const firstGameHour = document.getElementById(`firstGameHour`).value;
+      const secondGameHour = document.getElementById(`secondGameHour`).value;
+      const thirdGameHour = document.getElementById(`thirdGameHour`).value;
+      const newElement = document.getElementById(id);
+
+      if (newElement) {
+         newElement.innerHTML = `
+       <header>  
+         <p class="date"> ${setDate} </p>
+         <p id="dayOfWeek"> ${weekdays[day]} </p>
+       </header>
+  
+       <div class="games">
+         <div class="col-a">
+           <ul>
+             <li>
+               <button class="icon">
+                 <img src="${images[0]}" id="first" class="team"/>
+               </button>
+               <p id="firstGameHour"> ${firstGameHour} </p>
+               <button class="icon">
+                 <img src="${images[1]}" id="second" class="team" />
+               </button>
+             </li>
+           </ul>
+         </div>
+         <!-- 
+  
+         -->
+         <div class="col-b">
+           <ul>
+             <li>
+               <button class="icon">
+                 <img src="${images[2]}" id="third" class="team" />
+               </button>
+               <p id="secondGameHour"> ${secondGameHour} </p>
+               <button class="icon">
+                 <img src="${images[3]}" id="fourth" class="team" />
+               </button>
+             </li>
+           </ul>
+         </div>
+         <!-- 
+  
+         -->
+         <div class="col-c">
+           <ul>
+             <li>
+               <button class="icon">
+                 <img src="${images[4]}" id="fifth" class="team" />
+               </button>
+               <p id="thirdGameHour"> ${thirdGameHour} </p>
+               <button class="icon">
+                 <img src="${images[5]}" id="sixth" class="team" />
+               </button>
+             </li>
+           </ul>
+         </div>
+       </div>
+       <div class="modifyCardButtons">
+         <img src="./assets/delete.svg" onclick="deleteCard()" id="deleteButton">
+         <img src="./assets/edit.svg" onclick="showEditCard()" id="editButton">
+       </div>
+         </svg>`;
+
+         creator.style.visibility = 'hidden';
+         card.style.visibility = 'visible';
+         create.style.visibility = 'visible';
+         createButton.style.display = 'inline';
+         edit.style.display = 'none';
+         document.getElementById(`edit`).removeEventListener('click', createEditedCard);
+      }
+   };
+   document.getElementById(`edit`).addEventListener(`click`, createEditedCard);
+}
